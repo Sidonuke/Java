46f65de682a7fd6c4ba6a042c5ff02f4af7d0983
Sidonuke <Sidonuke@Sidonuke.net>
2016-06-14 10:24:37 -0500

    Add files via upload

---
+++ week2-027.FromOneToHundred/src/FromOneToHundred.java
@@ -0,0 +1,13 @@
+
+public class FromOneToHundred {
+
+    public static void main(String[] args) {
+        // Write your program here
+        int upward = 1;
+        
+            while (upward <= 100){
+                System.out.println(upward);
+                upward++;
+            }
+    }
+}
