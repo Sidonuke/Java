a2684a6a6115113ac8756eb8e86fac6e5684e7e1
Sidonuke <Sidonuke@Sidonuke.net>
2016-06-17 21:17:17 -0500

    Add files via upload

---
+++ week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class WordsInReverseOrder {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList<String>();
+        
+        while (true){
+            System.out.println("Type a word: ");
+            String word = reader.nextLine();
+            
+            if (word.isEmpty()){
+                Collections.reverse(words);
+                break;
+            } else{
+                words.add(word);
+            }
+        } for (String word : words){
+        System.out.println(word);
+        }
+    }
+}
