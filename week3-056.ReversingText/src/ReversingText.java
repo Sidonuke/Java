62fd8f72331ef87fb6d0de140d972886d22cc980
Sidonuke <Sidonuke@Sidonuke.net>
2016-06-17 20:46:17 -0500

    Add files via upload

---
+++ week3-056.ReversingText/src/ReversingText.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class ReversingText {
+
+    public static String reverse(String text) {
+        int j = text.length()-1;
+        int i = 0;
+        String reversed = "";
+        
+        while (j >= i){
+            reversed += "" + text.charAt(j);
+            j--;
+        }//System.out.println("In reverse order: " + stupid);
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Type in your text: ");
+        String text = reader.nextLine();
+        System.out.println("In reverse order: " + reverse(text));
+    }
+}
