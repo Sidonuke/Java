015fc4f3cad3c3fc0dae0b1e6cbaf61e088f8311
Sidonuke <Sidonuke@Sidonuke.net>
2016-06-29 20:47:03 -0500

    Add files via upload

---
+++ week4-072.1.YourFirstAccount/src/Accounts.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Accounts {
+
+    public static void main(String[] args) {
+        Account bartosAccount = new Account("Barto's account",100.00);
+
+        bartosAccount.deposit(20);
+        
+        System.out.println(bartosAccount);
+    }
+
+}
